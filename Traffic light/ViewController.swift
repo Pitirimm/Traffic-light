d87dcaad91326c978c7d7f2c273f97957a854421
Yuri Martinovsky <vvrite@me.com>
2021-05-18 23:29:41 +0300

    Added elements

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var orangeLightView: UIView!
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        greenLightView.alpha = 0.5
+        orangeLightView.alpha = 0.5
+        redLightView.alpha = 0.5
+        
+        greenLightView.layer.cornerRadius = 64
+        orangeLightView.layer.cornerRadius = 64
+        redLightView.layer.cornerRadius = 64
+        startButton.layer.cornerRadius = 15
     }
 
-
+    @IBAction func tappedStartButton() {
+        greenLightView.alpha = 1
+        startButton.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
