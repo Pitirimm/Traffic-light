557e511e777aba0e512bf06b34667cb6e851e5ec
Yuri Martinovsky <vvrite@me.com>
2021-05-19 15:54:23 +0300

    Added functionality

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -7,29 +7,61 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var greenLightView: UIView!
     @IBOutlet weak var orangeLightView: UIView!
     @IBOutlet weak var redLightView: UIView!
+    
     @IBOutlet weak var startButton: UIButton!
+    
+    enum TrafficLight {
+        case green
+        case orange
+        case red
+    }
+    
+    var lights:TrafficLight = .green
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       
         greenLightView.alpha = 0.5
         orangeLightView.alpha = 0.5
         redLightView.alpha = 0.5
         
-        greenLightView.layer.cornerRadius = 64
-        orangeLightView.layer.cornerRadius = 64
-        redLightView.layer.cornerRadius = 64
+        greenLightView.layer.cornerRadius = greenLightView.frame.height / 2
+        orangeLightView.layer.cornerRadius = orangeLightView.frame.height / 2
+        redLightView.layer.cornerRadius = redLightView.frame.height / 2
         startButton.layer.cornerRadius = 15
     }
 
     @IBAction func tappedStartButton() {
-        greenLightView.alpha = 1
         startButton.setTitle("NEXT", for: .normal)
+        
+        
+        switch lights {
+        case .green:
+            lights = TrafficLight.orange
+            greenLightView.alpha = 1
+            orangeLightView.alpha = 0.5
+            redLightView.alpha = 0.5
+        case .orange:
+            lights = TrafficLight.red
+            greenLightView.alpha = 0.5
+            orangeLightView.alpha = 1
+            redLightView.alpha = 0.5
+        case .red:
+                lights = TrafficLight.green
+                greenLightView.alpha = 0.5
+                orangeLightView.alpha = 0.5
+                redLightView.alpha = 1
+        
+        }
+        
     }
     
 }
